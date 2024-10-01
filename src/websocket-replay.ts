3a4f8e53f6be5ca3780c9873ca35ed2560a24d3f
Peter Petrov <onestone@gmail.com>
2024-10-01 04:36:37 +0300

    WebSocket servers

--- src/websocket-replay.ts
+++ src/websocket-replay.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { Command, InvalidArgumentError } from "@commander-js/extra-typings";
+import { WebSocketServer } from "ws";
 
 const program = new Command()
     .name("websocket-replay")
@@ -12,38 +13,61 @@ const program = new Command()
 program
     .command("record <url> <filename>")
     .description("Proxy WebSocket to <url> and record messages to <filename>")
-    .action((url: string, filename: string) => {
+    .action((url, filename) => {
+        const port = program.opts().port;
         console.log("Recording WebSocket messages to:", url, filename);
+        const wss = new WebSocketServer({ port }, () => {
+            console.log(`Websocket proxy server listening on http://localhost:${port}/`);
+        });
+        wss.on("connection", function connection(ws) {
+            ws.on("error", console.error);
+
+            ws.on("message", function message(data) {
+                console.log("received: %s", data);
+            });
+
+            ws.send("something");
+        });
     });
 
 program
     .command("replay <filename>")
     .description("Replay WebSocket messages from <filename>")
     .option("-s, --speed <speed>", 'Replay speed. Use "max" for no delays.', parseSpeed, 1)
-    .action((filename: string, options) => {
+    .action((filename, options) => {
         const port = program.opts().port;
-        const speedFactor = options.speed;
+        const speedFactor = options.speed === "max" ? 0 : 1 / options.speed;
         console.log("Replaying WebSocket messages from:", filename);
-        console.log("Options:", options);
-        console.log("Port:", port);
         console.log("Speed factor:", speedFactor);
+        const wss = new WebSocketServer({ port }, () => {
+            console.log(`Websocket replay server listening on http://localhost:${port}/`);
+        });
+        wss.on("connection", function connection(ws) {
+            ws.on("error", console.error);
+
+            ws.on("message", function message(data) {
+                console.log("received: %s", data);
+            });
+
+            ws.send("something");
+        });
     });
 
 program.parse();
 
-function parsePort(x: string): number {
+function parsePort(x: string) {
     const port = parseInt(x);
     if (!Number.isInteger(port) || port < 1 || port > 65535) {
-        throw new InvalidArgumentError(`Invalid port: ${x}`);
+        throw new InvalidArgumentError("Invalid port.");
     }
     return port;
 }
 
-function parseSpeed(x: string): number {
-    if (x === "max") return 0;
-    const speedFactor = 1 / parseFloat(x);
-    if (speedFactor < 0 || !Number.isFinite(speedFactor)) {
-        throw new InvalidArgumentError(`Invalid speed: ${x}`);
+function parseSpeed(x: string) {
+    if (x === "max") return "max";
+    const speed = parseFloat(x);
+    if (speed <= 0 || !Number.isFinite(speed)) {
+        throw new InvalidArgumentError("Invalid speed.");
     }
-    return speedFactor;
+    return speed;
 }
